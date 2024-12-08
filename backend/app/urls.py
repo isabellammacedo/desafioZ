b86c00a6b223d92e3a024fdc1e8866f52f1d064e
Isabella <isabellamacedocontato@gmail.com>
2024-12-08 00:09:28 -0300

    feat(backend): configura o Swagger

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,13 +2,27 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import CompanyViewSet, DocumentViewSet, SignersViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'Company', CompanyViewSet)
 router.register(r'Document', DocumentViewSet)
 router.register(r'Signers', SignersViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ZapSign API",
+        default_version='v1',
+        description="Documentação da API"
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)), 
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -3,13 +3,22 @@ from .models import Company, Document, Signers
 from .serializers import CompanySerializer, DocumentSerializer, SignersSerializer
 
 class CompanyViewSet(viewsets.ModelViewSet):
+    """
+    Dados da empresa.
+    """
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
 class DocumentViewSet(viewsets.ModelViewSet):
+    """
+    Documentos criados via API ZapSign.
+    """
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
 class SignersViewSet(viewsets.ModelViewSet):
+    """
+    Signatários associados a cada documento.
+    """
     queryset = Signers.objects.all()
     serializer_class = SignersSerializer
